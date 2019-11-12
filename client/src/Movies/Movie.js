921137d71970ae5be23e8d28e7a4004f2705ff14
miriam-sullivan <maljayd42@gmail.com>
2019-11-12 15:20:57 -0600

    added everything it seems to be working but im not sure if it looks right

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,57 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const Movie = (props) => {
-  const [movie, setMovie] = useState();
- 
-  useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
-
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-
-  },[]);
-  
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
-
-  if (!movie) {
-    return <div>Loading movie information...</div>;
+import React from "react";
+import MovieCard from "./MovieCard";
+import axios from "axios";
+export default class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: null
+    };
   }
 
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+  componentWillReceiveProps(newProps) {
+    if (this.props.match.params.id !== newProps.match.params.id) {
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
+  componentDidMount() {
+    this.fetchMovie(this.props.match.params.id);
+  }
+
+  fetchMovie = id => {
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        this.setState(() => ({ movie: response.data }));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie);
+  };
+
+  render() {
+    if (!this.state.movie) {
+      return <div>Loading movie information...</div>;
+    }
+
+    return (
+      <div className="save-wrapper">
+        <MovieCard movie={this.state.movie} />
+        <div className="save-button" onClick={this.saveMovie}>
+          Save
+        </div>
       </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Movie;
